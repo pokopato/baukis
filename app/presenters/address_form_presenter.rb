1f4fbf72bbeb43254cf29e0f70811e228399705e
pokopato <patora20140808@gmail.com>
2015-07-08 11:02:55 +0000

    Capybaraを使ったテスト

--- app/presenters/address_form_presenter.rb
+++ app/presenters/address_form_presenter.rb
@@ -4,6 +4,7 @@ class AddressFormPresenter < UserFormPresenter
       m << decorated_label(name, label_text, options)
       m << text_field(name, options)
       m.span '（7桁の半角数字で入力してください。）', class: 'notes'
+      m << error_messages_for(name)
     end
   end
 end
